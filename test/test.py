3f68a030a8eadbc097bafc961c35bb4acc8f27ff
Joanne Monaghan <joanne.currell@google.com>
2023-01-08 16:55:32 +0000

    unittest tidy

--- test/test.py
+++ test/test.py
@@ -8,6 +8,34 @@ class BlackjackTestCase(unittest.TestCase):
     def setUp(self):  # this method will be run before each test
         self.deck = Deck()
 
+#test deck starts with 52 cards
+    def test_initial_deck_size_is_52(self):
+        deck_size = len(self.deck.cards)
+        self.assertEqual(deck_size, 52)
+
+#test deal function deals one card
+    def test_deck_deals_card(self):
+        spec_deck = Deck()
+        spec_deck.cards = [('King', 'Diamonds'), ('Jack', 'Hearts'), (6, 'Hearts')]
+        my_hand = spec_deck.deal([])
+        self.assertEqual(my_hand, [(6, 'Hearts')])
+
+#test starting hand has two cards    
+    def test_starting_hand_has_two_cards(self):
+        spec_deck = Deck()
+        spec_deck.cards = [('King', 'Diamonds'), ('Jack', 'Hearts'), (6, 'Hearts')]
+        my_hand = spec_deck.start_game()
+        self.assertEqual(len(my_hand), 2)
+
+#check score has updated
+    def test_total_updates_after_hit(self):
+        spec_deck = Deck()
+        spec_deck.cards = [(3, 'Diamonds'), ('King', 'Diamonds'), (6, 'Hearts')]
+        my_hand = spec_deck.start_game()
+        my_hand = spec_deck.deal(my_hand)
+        self.assertEqual(total(my_hand), 19) 
+
+
 #test 21 is calculated with two cards
     def test_dealer_gets_21_with_two_cards(self):
         spec_deck = Deck()
@@ -16,8 +44,7 @@ class BlackjackTestCase(unittest.TestCase):
         self.assertEqual(my_result, 21)
 
 
-#test Ace counted as 1 and last. Hits on 16
-
+#test Ace counted as 1 and last (hits on 16 and updated)
     def test_ace_counted_as_1_and_last(self):
         spec_deck = Deck()
         spec_deck.cards = [(5, 'Diamonds'), (10, 'Diamonds'), (5, 'Spades'), ('Ace', 'Clubs')]
@@ -25,7 +52,6 @@ class BlackjackTestCase(unittest.TestCase):
         self.assertEqual(my_result, 21)
 
 #test Ace counted as 11. (stands after two cards)
-
     def test_ace_counted_as_11_and_last(self):
         spec_deck = Deck()
         spec_deck.cards = [(5, 'Diamonds'),('Jack', 'Spades'), ('Ace', 'Hearts')]
@@ -62,32 +88,7 @@ class BlackjackTestCase(unittest.TestCase):
         my_result = game(spec_deck, dealer_hit_or_stand)
         self.assertEqual(my_result, 26)
 
-#check score has updated
-    def test_score_updates_with_new_hand(self):
-        spec_deck = Deck()
-        spec_deck.cards = [(3, 'Diamonds'), ('King', 'Diamonds'), (6, 'Hearts')]
-        my_hand = spec_deck.start_game()
-        my_hand = spec_deck.deal(my_hand)
-        self.assertEqual(total(my_hand), 19) 
 
-#test deal function deals one card
-    def test_deck_deals_card(self):
-        spec_deck = Deck()
-        spec_deck.cards = [('King', 'Diamonds'), ('Jack', 'Hearts'), (6, 'Hearts')]
-        my_hand = spec_deck.deal([])
-        self.assertEqual(my_hand, [(6, 'Hearts')])
-
-#test starting hand has two card    
-    def test_starting_hand_has_two_cards(self):
-        spec_deck = Deck()
-        spec_deck.cards = [('King', 'Diamonds'), ('Jack', 'Hearts'), (6, 'Hearts')]
-        my_hand = spec_deck.start_game()
-        self.assertEqual(len(my_hand), 2)
-
-#test deck starts with 52 cards
-    def test_initial_deck_size_is_52(self):
-        deck_size = len(self.deck.cards)
-        self.assertEqual(deck_size, 52)
 
 #calculate winner function tests
     
@@ -146,7 +147,6 @@ class BlackjackTestCase(unittest.TestCase):
         self.assertFalse(spec_moneypot.can_play())
 
 #test when moneypot is above 0, player can choose to play
-
     def test_can_play_if_moneypot_positive(self):
         spec_moneypot = MoneyPot()
         self.assertTrue(spec_moneypot.can_play())
@@ -154,59 +154,3 @@ class BlackjackTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-""" 
-Given I play a game of blackjack
-When I am dealt my opening hand
-Then I have two cards
-
-def test_opening_hand_is_two(self):
-    self.assertEqual(opening_hand, 2)
-
-test hand calculation
-with A/K/Q/J
-
-Given I have a valid hand of cards
-When I choose to ‘hit’
-Then I receive another card
-And my score is updated
-
-Given I have a valid hand of cards
-When I choose to ‘stand’
-Then I receive no further cards
-And my score is evaluated
-
-Given my score is updated or evaluated
-When it is 21 or less
-Then I have a valid hand
-
-def test_valid_hand_<21(self):
-    self.assertTrue(valid_hand, 20)
-
-def test_valid_hand_=21(self):
-    self.assertTrue(valid_hand, 21)
-
-Given my score is updated
-When it is 22 or more 
-Then I am ‘bust’ and do not have a valid hand
-
-def test_valid_hand_false_=>22(self)
-    self.assertFalse(valid_hand, 22)
-
-Given I have a king and an ace
-When my score is evaluated
-Then my score is 21
-
-Given I have a king, a queen, and an ace
-When my score is evaluated
-Then my score is 21
-
-Given that I have a nine, an ace, and another ace
-When my score is evaluated
-Then my score is 21	
-
-suit = 13cards
-52cards
-"""
-
